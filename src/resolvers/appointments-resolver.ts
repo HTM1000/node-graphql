c49af0b346b41c37c21dcfbef2922ca24aff70d5
HTM1000 <ID1355342@outlook.com>
2022-10-03 20:34:55 -0300

    node + graphql

---
+++ src/resolvers/appointments-resolver.ts
@@ -0,0 +1,34 @@
+import { Customer } from './../dtos/models/customer-model';
+import { Appointment } from './../dtos/models/appointment-model';
+import { Resolver, Query, Mutation, Arg, FieldResolver, Root } from 'type-graphql';
+import { CreateAppointmentInput } from '../dtos/inputs/create-appointment-input';
+
+@Resolver(() => Appointment)
+export class AppointmentResolver {
+  @Query(() => [Appointment])
+  async appointments() {
+    return [{
+      startsAt: new Date,
+      endsAt: new Date,
+    }]
+  }
+
+  @Mutation(() => Appointment)
+  async createAppointment(@Arg('data') data: CreateAppointmentInput) {
+     const appointment = {
+      startsAt: data.startsAt,
+      endsAt: data.endsAt,
+     }
+
+     return appointment;
+  }
+
+
+  @FieldResolver(() => Customer)
+  async customer(@Root() appointment: Appointment) {
+
+    return {
+      name: 'Henrique Moraca'
+    }
+  } 
+}
